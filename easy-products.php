c0cc455ecf2ade9ddb837b092085177de35a8675
Bryce Meyer <bryce13950@gmail.com>
2017-01-24 10:57:59 -0600

    added easy products hook file

---
+++ easy-products.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Easy Products
+ * Plugin URI: https://github.com/pomeloproductions/easy-products
+ * Version: 0.1.0
+ * Author: Pomelo Productions
+ * Description: Easily manage products, and allow users to add products to a shopping cart
+ * Text Domain: easy-products
+ * License: GPLv3
+ */
