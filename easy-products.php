05d340fc07b42d05dedb03f901792aff1a6a8f7c
Bryce Meyer <bryce13950@gmail.com>
2017-01-24 11:17:47 -0600

    initiated WordWrap

--- easy-products.php
+++ easy-products.php
@@ -8,3 +8,30 @@
  * Text Domain: easy-products
  * License: GPLv3
  */
+
+namespace EasyProducts;
+
+use WordWrap;
+
+function showInstallErrorMessage(){
+    echo '<div class="error"><p>Sorry, but Easy Products requires Word Wrap to be installed and active.</p></div>';
+}
+
+function hasWordWrap() {
+    if ( is_admin() && current_user_can( 'activate_plugins' ) &&  !is_plugin_active( 'word-wrap/word-wrap.php' ) ) {
+        add_action( 'admin_notices', '\EasyProducts\showInstallErrorMessage' );
+
+        deactivate_plugins( plugin_basename( __FILE__ ) );
+
+        if ( isset( $_GET['activate'] ) ) {
+            unset( $_GET['activate'] );
+        }
+    }
+}
+
+add_action( 'admin_init', '\EasyProducts\hasWordWrap' );
+
+require ('vendor/autoload.php');
+
+include_once(__DIR__ . '/../word-wrap/word-wrap.php');
+WordWrap::init(basename(__DIR__));
