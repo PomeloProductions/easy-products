467ca5452b2a4dadd379ab90f34c0dbb874d23ac
Bryce Meyer <bryce@axolotlinteractive.com>
2017-02-04 16:43:00 -0600

    added region model

---
+++ classes/Model/Region.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bryce
+ * Date: 2/4/17
+ * Time: 4:26 PM
+ */
+
+namespace EasyProducts\Model;
+
+use WordWrap\ORM\BaseModel;
+
+class Region extends BaseModel
+{
+
+    /**
+     * @var The name of the region
+     */
+    public $name;
+
+    /**
+     * Overwrite this in your concrete class. Returns the table name used to
+     * store models of this class.
+     *
+     * @return string the table name without a prefix
+     */
+    public static function getTableName() {
+        return "easy_products_regions";
+    }
+
+    /**
+     * Get an array of fields to search during a search query.
+     *
+     * @return array
+     */
+    public static function getSearchableFields() {
+        // TODO: Implement get_searchable_fields() method.
+    }
+
+    /**
+     * Get an array of all fields for this Model with a key and a value
+     * The key should be the name of the column in the database and the value should be the structure of it
+     *
+     * @return array
+     */
+    public static function getFields() {
+        return [
+            'name' => 'VARCHAR(255)'
+        ];
+    }
+}
