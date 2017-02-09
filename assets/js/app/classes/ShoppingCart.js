3aeec063b5827d0be7aa354ecc57c90954e74ec4
Bryce Meyer <bryce@axolotlinteractive.com>
2017-02-08 19:24:32 -0600

    setup new products

--- assets/js/app/classes/ShoppingCart.js
+++ assets/js/app/classes/ShoppingCart.js
@@ -4,7 +4,19 @@
 
 class ShoppingCart {
 
+    /**
+     * Constructor for this shopping cart instance
+     *
+     * @param productsForm the dom form
+     */
     constructor(productsForm) {
         this.productsForm = productsForm;
+        this.products = [];
+
+        let productContainers = this.productsForm.getElementsByClassName('easy_products-product');
+
+        for (let i = 0; i < productContainers.length; i++) {
+            this.products.push(new Product(productContainers[i]));
+        }
     }
 }
