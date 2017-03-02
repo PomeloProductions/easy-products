bcd3d9ee2137920fd429680040d037e99a5369c5
Bryce Meyer <bryce@axolotlinteractive.com>
2017-03-01 19:52:07 -0600

    Revert "changed shipped toggle usage"
    This reverts commit 97def4ac443fa12f217054f29ede19cdde4ebebf.

--- classes/Admin/Orders/ViewAll.php
+++ classes/Admin/Orders/ViewAll.php
@@ -48,6 +48,16 @@ class ViewAll extends TaskController
      */
     protected function renderMainContent() {
 
+        $orders = [];
+
+        foreach ($this->orders as $order) {
+            $orders[] = [
+                'id' => $order->id,
+                'name' => $order->name,
+                'shipped_message' => $order->shipped ? 'Shipped' : 'Not Shipped'
+            ];
+        }
+
         $links = '';
 
         $i = 0;
@@ -66,7 +76,7 @@ class ViewAll extends TaskController
         }
 
         $data = [
-            "orders" => $this->orders,
+            "orders" => $orders,
             "page_links" => $links,
             "first_page" => $this->page == 0,
             "previous_page" => $this->page,
