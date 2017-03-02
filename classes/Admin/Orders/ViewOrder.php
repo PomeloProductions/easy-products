bc6dfa4bdca3062fcbfda2f7ec0342083b978b94
Bryce Meyer <bryce@axolotlinteractive.com>
2017-03-01 20:17:59 -0600

    setup view order page

--- classes/Admin/Orders/ViewOrder.php
+++ classes/Admin/Orders/ViewOrder.php
@@ -9,24 +9,41 @@
 namespace EasyProducts\Admin\Orders;
 
 
+use EasyProducts\Model\Order;
+use Exception;
 use WordWrap\Admin\TaskController;
+use WordWrap\Assets\Template\Mustache\MustacheTemplate;
 
 class ViewOrder extends TaskController
 {
 
     /**
+     * @var Order The order we are viewing
+     */
+    private $order;
+
+    /**
      * override this to setup anything that needs to be done before
      * @param $action string the action the user is trying to complete
+     * @throws Exception
      */
     public function processRequest($action = null) {
-        // TODO: Implement processRequest() method.
+
+        $this->order = Order::find_one($_GET['id']);
+
+        if (!$this->order) {
+            throw new Exception('Order not found');
+        }
     }
 
     /**
      * override to render the main page
      */
     protected function renderMainContent() {
-        // TODO: Implement renderMainContent() method.
+
+        $template = new MustacheTemplate($this->lifeCycle, "admin/orders/view_order", $this->order);
+
+        return $template->export();
     }
 
     /**
