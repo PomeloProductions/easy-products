08796fffe0764734f2aafaee0a9b39d4123e3878
Bryce Meyer <bryce13950@gmail.com>
2017-03-10 04:17:44 -0600

    added save processing

--- classes/Admin/CreateProduct.php
+++ classes/Admin/CreateProduct.php
@@ -9,9 +9,22 @@
 namespace EasyProducts\Admin;
 
 
-use EasyProducts\Model\Product;
 
 class CreateProduct extends ViewProduct
 {
 
+
+    /**
+     * override this to setup anything that needs to be done before
+     * @param $action string the action the user is trying to complete
+     */
+    public function processRequest($action = null)
+    {
+        parent::processRequest($action);
+
+        if ($action == 'save') {
+            header('Location: /wp-admin/admin.php?page=easy_products&task=view_product&id=' . $this->product->id);
+            exit;
+        }
+    }
 }
