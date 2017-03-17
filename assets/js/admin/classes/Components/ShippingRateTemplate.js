218d855f3927d7842186a148d927c07e461245ed
Bryce Meyer <bryce@axolotlinteractive.com>
2017-03-17 13:54:59 -0400

    added shipping rate template class

---
+++ assets/js/admin/classes/Components/ShippingRateTemplate.js
@@ -0,0 +1,36 @@
+/**
+ * Created by bryce on 3/17/17.
+ */
+
+export class ShippingRateTemplate {
+
+    /**
+     *
+     *
+     * @param templateContent Node
+     */
+    constructor(templateContent) {
+
+        this.template = templateContent.cloneNode(true);
+
+        let regionSelect = this.template.querySelector('.shipping_rate-region');
+
+        regionSelect.selectedIndex = 0;
+
+        let inputs = this.template.getElementsByTagName('input');
+
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].value = '';
+        }
+
+    }
+
+    /**
+     * Clones the template, so a new node can be added to the page
+     *
+     * @returns {Node}
+     */
+    cloneTemplate () {
+        return this.template.cloneNode(true);
+    }
+}
