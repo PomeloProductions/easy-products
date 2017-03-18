ef1558b746da854a602af4f06f9cac5509936831
Bryce Meyer <bryce@axolotlinteractive.com>
2017-03-17 23:56:51 -0400

    made submission work properly

--- assets/js/admin/classes/ProductsList.js
+++ assets/js/admin/classes/ProductsList.js
@@ -8,11 +8,35 @@ export class ProductsList {
 
     constructor(productsTable) {
 
-        let options = {
-            perPageSelect: []
-        };
+        this.products = document.getElementsByClassName('crud_rows');
         jQuery(productsTable).find('tbody').sortable({
             itemSelector: 'tr'
         });
+
+        jQuery(productsTable).find('tr').on('mouseup', this.reorderedProducts.bind(this));
+        jQuery(productsTable).find('tr').on('touchend', this.reorderedProducts.bind(this));
+    }
+
+    /**
+     * Reorders the products on the server
+     */
+    reorderedProducts () {
+
+        setTimeout(function() {
+            let newOrder = [];
+
+            for (let i = 0; i < this.products.length; i++) {
+                newOrder.push(this.products[i].dataset['id']);
+            }
+
+            console.log(newOrder);
+            jQuery.ajax('/wp-admin/admin-ajax.php', {
+                method: 'POST',
+                data: {
+                    action: 'reorder_products',
+                    order: newOrder
+                }
+            });
+        }.bind(this), 10);
     }
 }
